76f37b6b210d98cff429a5e8adff2d7557b675c9
Edwin.Lim <elim@sapient.com>
2019-03-01 10:32:34 +0800

    - eslint - models update - update index.js for training

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,20 @@
 module.exports = {
-    "extends": "airbnb-base"
+  extends: 'airbnb-base',
+  rules: {
+    'no-console': 'off',
+    'max-len': [
+      'error',
+      {
+        'code': 200,
+        'ignoreComments': true,
+      }
+    ],
+    'implicit-arrow-linebreak': [
+      'error', 'beside'
+    ],
+    'no-nested-ternary': 0
+  },
+  globals: {
+    fetch: true,
+  },
 };
--- index.js
+++ index.js
@@ -1,48 +1,112 @@
-import 'babel-polyfill';
-import path from 'path';
-import fs from 'fs';
-import moment from 'moment';
-import * as _ from 'lodash';
-import * as faceapi from 'face-api.js';
-import * as commons from './commons';
+// import 'babel-polyfill';
+// import 'isomorphic-fetch';
 
-global.fetch = require('node-fetch');
+// global.fetch = require('node-fetch');
+const path = require('path');
+// import fs from 'fs';
+// import moment from 'moment';
+const tf = require('@tensorflow/tfjs-node');
+const _ = require('lodash');
+const faceapi = require('face-api.js');
+const canvas = require('canvas');
+const fs = require('fs');
+const commons = require('./commons');
 
-// console.log('faceapi', faceapi);
+const facesLabelledPath = './dist/faces_labelled';
+const { Canvas, Image, ImageData } = canvas;
+faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
 
-const options = {
-  concurrent: 8,
-  inputPath: './faces_labelled/',
-  ouput: './model.json',
+const faceDetectionNet = faceapi.nets.ssdMobilenetv1;
+// SsdMobilenetv1Options
+const minConfidence = 0.5;
+
+// TinyFaceDetectorOptions
+const inputSize = 408;
+const scoreThreshold = 0.5;
+
+// MtcnnOptions
+const minFaceSize = 50;
+const scaleFactor = 0.8;
+
+tf.disableDeprecationWarnings();
+
+const getFaceDetectorOptions = net => (net === faceapi.nets.ssdMobilenetv1
+  ? new faceapi.SsdMobilenetv1Options({ minConfidence })
+  : net === faceapi.nets.tinyFaceDetector
+    ? new faceapi.TinyFaceDetectorOptions({ inputSize, scoreThreshold })
+    : new faceapi.MtcnnOptions({ minFaceSize, scaleFactor }));
+
+const faceDetectionOptions = getFaceDetectorOptions(faceDetectionNet);
+
+const saveFile = (fileName, buff) => {
+  const baseDir = './output';
+  if (!fs.existsSync(baseDir)) {
+    fs.mkdirSync(baseDir);
+  }
+
+  fs.writeFileSync(path.resolve(baseDir, fileName), buff);
 };
 
-init = async () => {
-  // await faceapi.loadSsdMobilenetv1Model('/models');
-  // console.log(faceapi.nets);
+const init = async () => {
+  await faceDetectionNet.loadFromDisk('./src/models/');
+  await faceapi.nets.faceLandmark68Net.loadFromDisk('./src/models/');
+  await faceapi.nets.faceRecognitionNet.loadFromDisk('./src/models/');
 
-  const imagesByPath = (await fetch('dir.txt').then(res => res.text()))
-    .split('\n')
-    .map(path => path.replace('./dist/', './'));
+  const imagesByPath = fs
+    .readdirSync(facesLabelledPath)
+    .filter(p => !p.includes('DS_Store'))
+    .map(p => path.join(facesLabelledPath, p));
 
-  const filepaths = await Promise.all(
-    commons.names.map(
-      async name => await Promise.all(
-        imagesByPath
-          .filter(path => path.includes(name))
-          .map(path => faceapi.fetchImage(path).then(res => res)),
-      ),
-    ),
+  const imagesArr = await Promise.all(
+    commons.names.map(async (name) => {
+      const filter = imagesByPath.filter(p => p.includes(name)).map(async p => canvas.loadImage(p));
+      return Promise.all(filter);
+    }),
   );
 
   const imagesByNames = {};
-  commons.names.forEach((name, nameIndex) => (imagesByNames[name] = filepaths[nameIndex]));
-  console.log('imagesByNames', imagesByNames);
+  commons.names.forEach((name, nameIndex) => {
+    imagesByNames[name] = imagesArr[nameIndex];
+  });
 
-  
-  const descriptorByNames = await _.mapValues(imagesByNames, async images => await Promise.all(images.map(image => faceapi.detectSingleFace(image).withFaceLandmarks().withFaceDescriptor())));
-  // console.log('descriptorByNames1', imagesByNames['Adarsh Mysore Chandrashekar'][0]);
-  // console.log('descriptorByNames1', await faceapi.detectSingleFace(imagesByNames['Adarsh Mysore Chandrashekar'][0]).withFaceLandmarks().withFaceDescriptor());
-  console.log('descriptorByNames2', descriptorByNames);
+  const descriptorFaceArr = await Promise.all(
+    imagesArr.map(async (images) => {
+      const map = images.map(async image => faceapi
+        .detectSingleFace(image, faceDetectionOptions)
+        .withFaceLandmarks()
+        .withFaceDescriptor());
+      return Promise.all(map);
+    }),
+  );
+  const descriptorArr = descriptorFaceArr.map(descriptorResults => _.reduce(
+    descriptorResults,
+    (arr, descriptorResult) => {
+      if (descriptorResult !== undefined) {
+        try {
+          arr.push(descriptorResult.descriptor);
+        } catch (e) {
+          console.log(e);
+        }
+      }
+      return arr;
+    },
+    [],
+  ));
+
+  const descriptorByNames = {};
+  commons.names.forEach((name, nameIndex) => {
+    if (descriptorArr[nameIndex].length) {
+      descriptorByNames[name] = descriptorArr[nameIndex];
+    }
+  });
+
+  console.log(`Building FaceMatcher from ${Object.keys(descriptorByNames).length} results`);
+  const labeledDescriptors = _.map(descriptorByNames, (descriptors, name) => new faceapi.LabeledFaceDescriptors(name, descriptors));
+  saveFile('labeledDescriptors.json', JSON.stringify(labeledDescriptors));
+
+  // const faceMatcher = new faceapi.FaceMatcher(labeledDescriptors);
+  // console.log('faceMatcher0', faceMatcher.findBestMatch(descriptorByNames['Hanis Adibah Yusof'][0]).toString());
+  // console.log('faceMatcher2', faceMatcher.findBestMatch(descriptorByNames['Jose Mella'][0]).toString());
 };
 
 init();
